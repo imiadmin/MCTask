d40220c6e58a7f182eb02bdd5c621ef99dab550b
コンビニさん <mamaokawari@gmail.com>
2015-08-30 14:38:01 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>MCTask|さと鯖</title>
+</head>
+<body>
+<h1>さとーさーばー管理システム</h1>
+<script type="text/javascript" src="http://mc.erinn.biz/intl/polling.js.php?server_addr=sato-server.ddo.jp&server_port=25565&size=0"></script><br>
+<a href="youtube.html">参加者のYoutubeチャンネル</a><br>
+<a href="info.html">関連情報・お知らせ</a><br>
+</body>
+<html>
